ff7c9d0a30dbf84da8f0af664c75eb2f212231c3
Abel <ghazinyan.abel@gmail.com>
2017-07-11 22:28:48 +0400

    PSR-1/2 added

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: abelghazinyan
- * Date: 7/9/17
- * Time: 5:59 PM
- */
+
 echo "<ul><li>2</li>";
-for($j = 3; $j <= 100; $j++){
-    for($i = 2; $i <= floor(sqrt($j)); $i++) {
+
+for ($j = 3; $j <= 100; $j++){
+    for ($i = 2; $i <= floor(sqrt($j)); $i++) {
         if ($j % $i == 0) {
             continue 2;
         }
     }
-        echo "<li>{$j}</li>";
+    echo "<li>{$j}</li>";
 }
+
 echo "</ul>";
